3d5b665beb18220c220df1e13e1112d5550406de
lvlFarrukh <47663235+lvlFarrukh@users.noreply.github.com>
2020-06-11 11:03:39 -0700

    Add files via upload

---
+++ Assignment N0 1/app.js
@@ -0,0 +1,328 @@
+// // ASSIGNMENT NO 1
+// //  JAVASCRIPT
+
+
+// // CHAPTER N0 1
+
+// // 1
+// alert("Good Morning User");
+
+// // 2
+// alert("Error! Please enter avalid password");
+
+// // 3
+// alert("Welcome to js land...\nHappy Coding!")
+
+// // 4
+// alert("Welcome to js land");
+
+// // alert("<h3>Pakistan<h3>");
+
+// // 5
+// alert("Hello... I can run JS through my web browser's console");
+ 
+
+// // CHAPTER N0 2
+
+// // 1
+// var username;
+
+// // 2
+// var myname = "FARRUKH ASLAM";
+
+// // 3
+// var message;
+// message = "PAKISTAN ZINDABAD";
+// alert(message);
+
+// // 4
+// alert(myname);
+// var age = 23;
+// alert(age + " year old");
+// alert("Certified Mobile Application Development 'INSHALLAH'");
+
+// // 5
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// // 6
+// email = "farrukha303@gmail.com";
+// alert("My email address is " + email);
+
+// // 7
+// var txt = "A smarter way to learn JavaScript";
+// alert("I am rying to learn from book " + txt);
+
+// // 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// // 9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+// // CHAPTER N0 3
+
+// // 1
+// var age = "23";
+// alert("I am " +age+ " year old");
+
+// // 2
+// var n = "7"
+// alert("You have visited this site " +n+ "times");
+
+// // 3
+// var dobY = 1997;
+// document.write("<br><br> date of birth year " +dobY);
+// document.write("<br>Data type of my declared variable is " +typeof dobY);  
+
+// // 4
+// var visitorName = "Ali";
+// var productTitle = "Suit";
+// var quantity = 7;
+
+// document.write("<br><br>" +visitorName+ " order " +quantity+ " " +productTitle+ " from j.");
+
+
+
+// // CHAPTER N0 4
+
+// // 1
+// var n1,n2,n3;
+
+// // 2
+// var first_name, firstName, sum_, div_num, total;
+
+// // var $name, *first_name, &abc, .abcd, @abc_n;
+
+// // 3
+// document.write("<br><br><h2>Rules for naming JS variables</h2>");
+// document.write("<br>Variable names can only contain number, $, and _. For example $my_1stVariable");
+// document.write("<br>Variables must begin with a letter , $ or _. For example $name, _name or name");
+// document.write("<br>Rules for naming JS variables");
+// document.write("<br>Variable names are case sensitive");
+// document.write("<br>Variable names should not be JS keywords");
+
+
+
+// // CHAPTER N0 5
+
+// // 1
+// var n1 = 3;
+// var n2 = 5;
+// var sum = n1 + n2;
+
+// document.write("<br>Sum of " +n1+ " & " +n2+ " is " +sum);
+
+// // 2
+// var sub = n1-n2;
+// document.write("<br>Subtraction of " +n1+ " & " +n2+ " is " +sub);
+// document.write("<br>Multiplication of " +n1+ " & " +n2+ " is " +n1*n2);
+// document.write("<br>Division of " +n1+ " & " +n2+ " is " +n1/n2);
+// document.write("<br>Modulus of " +n1+ " & " +n2+ " is " +n1%n2);
+
+// // 3
+// var num;
+// document.write("<br><br>The value after declaration is "+num);
+// num = 5;
+// document.write("<br>Initial value "+num);
+// document.write("<br>Value after increased is "+ ++num);
+// num = num + 7;
+// document.write("<br>Value after addition is "+ num);
+// document.write("<br>Value after decrement is "+ --num);
+// num = num % 2;
+// document.write("<br>The remainder is "+ num%3);
+
+// // 4
+// var ticket_prize = 600;
+// var n = 5
+// document.write("<br><br>Total cost to buy " +n+ " tickets to a movie is " +n*ticket_prize+ "PKR");
+
+// // 5
+// var table = 4;
+// document.write("<br><br>Table of " +table);
+// for (var index = 1; index < 11; index++) {
+//     document.write("<br>"+table+ " X "+index+" = "+table*index);
+// }
+
+// // 6
+// var c = 25;
+// document.write("<br><br>"+c+ "oC is "+ ((c*9/5)+32) + "oF");
+// var f = 70;
+// document.write("<br>"+f+ "oF is "+(f-32)*5/9+ "oC");
+
+// // 7
+// var item1 = 650;
+// var q1 = 3;
+// var item2 = 100;
+// var q2 = 7;
+// var s_c = 100
+// document.write("<br><br><h1>Shopping Cart</h1>");
+// document.write("<br>Prize of item 1 is "+item1);
+// document.write("<br>Quantity of item 1 is "+q1);
+// document.write("<br>Prize of item 2 is "+item2);
+// document.write("<br>Quantity of item 2 is "+q2);
+// var total = item1*q1 + item2*q2 + s_c;
+// document.write("<br>Total cost of your order is "+ total);
+
+
+// // 8
+// document.write("<br><br><h1>Mark Sheet</h1>");
+// var t_m = 980;
+// var m_o = 804;
+// var p = m_o/t_m * 100; 
+// document.write("<br>Total marks "+t_m);
+// document.write("<br>Obtain marks "+m_o);
+// document.write("<br>Percentage "+p);
+
+// // 9
+// document.write("<br><br><h1>Currency in PKR</h1>");
+// var d = 10;
+// var r = 25;
+// var t = 104.80*d + 28*25;
+// document.write("<br>total currency in PKR is "+t);
+
+// // 10
+// var num = 7;
+// num = num+5*10/2;
+// document.write("<br><br>After mathemetical expression ans: "+num);
+
+// // 11
+// document.write("<br><br><h1>AGE CALCULATOR</h1>");
+// var dY = 1997;
+// var cY = 2020;
+// document.write("<br>Current Year "+cY);
+// document.write("<br>Birth Year "+dY);
+// var age = cY-dY;
+// document.write("<br>Your age "+age);
+
+// // 12
+// var radius = 20;
+// document.write("<br><br><h1>The Geometrizer</h1>");
+// document.write("<br>Radius of a circle "+radius);
+// document.write("<br>The circumference is "+ radius*3.14*2);
+// document.write("<br>The area is "+ radius*radius*3.14);
+
+// // 13
+// var snck = "oreo";
+// var cAge = 23;
+// var maxAge = 77;
+// var perDay = 2;
+// document.write("<br><br><h1>The Lifetime Supply Calculator</h1>");
+// document.write("<br>Favourite Snack: "+ snck);
+// document.write("<br>Current age: "+ cAge);
+// document.write("<br>Estimated Maximum Age"+ maxAge);
+// document.write("<br>Amount of snacks per day "+ perDay);
+// var tS = (maxAge - cAge) * 2;
+// document.write("<br>You will need "+ tS + " " +snck+ " to last you until the rip old age of "+maxAge);
+
+
+
+// // CHAPTER N0 6-9
+
+// // 1
+// document.write("<br><br>Result");
+// var a = 10;
+// document.write("<br>The value of a is: "+a);
+// document.write("<br>-----------------------");
+// document.write("<br><br>The value of ++a is: "+ ++a);
+// document.write("<br>Now the value of a is: "+a);
+// document.write("<br><br>The value of a++ is: "+ a++);
+// document.write("<br>Now the value of a is: "+a);
+// document.write("<br><br>The value of --a is: "+ --a);
+// document.write("<br>Now the value of a is: "+a);
+// document.write("<br><br>The value of a-- is: "+ a--);
+// document.write("<br>Now the value of a is: "+a);
+
+// // 2
+// var a = 2, b = 1;
+// var result =  --a - --b + ++b + b--;
+
+// // Explain the output at each stage:
+// // --a;
+//     // ans: 1
+
+// // --a - --b;
+//     // ans: 0
+
+// // --a - --b + ++b;
+//     // ans: 0
+
+// // --a - --b + ++b + b--;
+//     // ans: -1
+
+
+// // 3
+// var name = prompt("What your name: ");
+// alert("Assalam o alikum "+name);
+
+// // 5
+// var n = prompt("Enter Number for Display table");
+// document.write("<br><br><h2>Table</h2>");
+
+// if(n == ""){
+//     for (let index = 1; index < 11; index++) {
+//         document.write("<br> 5 X " +index+ " = "+index*5);
+//     }
+// }
+// else{
+//     for (let index = 1; index < 11; index++) {
+//         document.write("<br> "+n+" X " +index+ " = "+index*n);
+//     }
+// }
+
+// // 6
+// var s1,s2,s3,o1,o2,o3,t = 100;
+// s1 = prompt("Enter Subject 1 name ");
+// o1 = prompt("Enter its Obaining Number");
+// s2 = prompt("Enter Subject 2 name ");
+// o2 = prompt("Enter its Obaining Number");
+// s3 = prompt("Enter Subject 3 name ");
+// o3 = prompt("Enter its Obaining Number");
+
+// document.write("<table><th><td><h3>Subject</h3></td><td><h3>Total Marks</h3></td><td><h3>Obtain Marks</h3></td><td><h3>Percentage</h3></td></th>");
+// document.write("<tr><td></td><td>"+s1+"</td><td>"+t+"</td><td>"+o1+"</td><td>"+ o1/t*100 +"</td></tr>");
+// document.write("<tr><td></td><td>"+s2+"</td><td>"+t+"</td><td>"+o2+"</td><td>"+ o2/t*100 +"</td></tr>");
+// document.write("<tr><td></td><td>"+s3+"</td><td>"+t+"</td><td>"+o3+"</td><td>"+ o3/t*100 +"</td></tr>");
+// var total_m = parseInt(o1) + parseInt(o2) + parseInt(o3);
+// document.write("<tr><td></td><td></td><td align='center'><strong>"+t*3+"</strong></td><td align='center'><strong>"+ total_m +"</strong></td><td align='center'><strong>"+ (o1/t*100 + o3/t*100 + o2/t*100)/3 +"</strong></td></tr></table>");
+
+
+
+// CHAPTER N0 9-11
+
+// 1
+var city = prompt("Enter Your city name");
+if(city.toLowerCase() == "karachi") {
+    alert("Welcome to city of light");
+}
+else{
+    alert("Welcome to "+city);
+}
+
+// 2
+var gender = prompt("Enter Your gender");
+if(gender.toLowerCase() == "male") {
+    alert("Good Morning Sir");
+}
+if(gender.toLowerCase() == "female"){
+    alert("Good Morning Mam");
+}
+
+// 3
+var color = prompt("Enter Color");
+if (color.toLowerCase() == "red") {
+    alert("Must Stop");
+}
+if (color.toLowerCase() == "yellow") {
+    alert("Ready to move");
+}
+if (color.toLowerCase() == "green") {
+    alert("Move now");
+}
+
+// 4
+var fuel = prompt("Input remaining fuel in car");
+if (parseTo(fuel) < 0.25 ){
+    alert("Please refill the fuel in your car”");
+}
+
+// 5
